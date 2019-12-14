753b1491dc3d775419adb97160c278b454e8a5dd
shevavm <shevavm@gmail.com>
2019-12-14 09:16:51 +0200

    fix extentions

---
+++ app/src/main/java/ru/skillbranch/kotlinexample/extentions/Iterable.kt
@@ -0,0 +1,13 @@
+package ru.skillbranch.kotlinexample.extentions
+
+fun <T> List<T>.dropLastUntil(predicate: (T) -> Boolean): List<T> {
+    if (!isEmpty()) {
+        val iterator = listIterator(size)
+        while (iterator.hasPrevious()) {
+            if (predicate(iterator.previous())) {
+                return take(iterator.nextIndex())
+            }
+        }
+    }
+    return emptyList()
+}
